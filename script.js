a7463236a7c162384592640881d47843844c18a1
zunairjarry5-a11y <zunairjarry5@gmail.com>
2026-03-29 01:51:56 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// Start Earning button action
+function startEarning() {
+alert("Welcome! Please register to start earning.");
+}
+
+// Login button action
+function goLogin() {
+alert("Redirecting to Login Page...");
+}
+
+// Register button action
+function goRegister() {
+alert("Redirecting to Register Page...");
+}
+
+// Simple animation when page loads
+window.onload = function(){
+console.log("Website loaded successfully!");
+}
